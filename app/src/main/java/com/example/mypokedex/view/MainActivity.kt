eea6863b76b5fb8309e2c308e96851f9e70a0847
Hugo Jordan <hugoj8366@gmail.com>
2024-01-23 14:44:29 -0300

    Sincronizando navController e appBarConfiguration

--- app/src/main/java/com/example/mypokedex/view/MainActivity.kt
+++ app/src/main/java/com/example/mypokedex/view/MainActivity.kt
@@ -2,11 +2,48 @@ package com.example.mypokedex.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.mypokedex.R
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var toolbar: Toolbar
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navigationView: NavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findAllElements()
+        setSupportActionBar(toolbar)
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.home_fragment,R.id.unknown_pokemon_fragment,R.id.about_fragment),
+            drawerLayout
+        )
+        navigationView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController,appBarConfiguration)
+    }
+
+    private fun findAllElements(){
+        toolbar = findViewById(R.id.nav_toolbar)
+        navController = findNavController(R.id.main_nav_host_fragment)
+        drawerLayout = findViewById(R.id.main_drawer_layout)
+        navigationView = findViewById(R.id.navigation_view)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
