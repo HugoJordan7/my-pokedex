ae339fd86a25d94d81b4f11386a78d088e39d16d
Hugo Jordan <hugoj8366@gmail.com>
2024-02-05 11:21:28 -0300

    Finalizando fragmento de informações do app

--- app/src/main/java/com/example/mypokedex/view/AboutFragment.kt
+++ app/src/main/java/com/example/mypokedex/view/AboutFragment.kt
@@ -4,15 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
+import com.example.mypokedex.BuildConfig
 import com.example.mypokedex.R
 
 class AboutFragment: Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_about,container,false)
@@ -20,7 +18,8 @@ class AboutFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val version = getString(R.string.build_version,BuildConfig.VERSION_NAME)
+        view.findViewById<TextView>(R.id.app_version_text).text = version
     }
-
 
 }
